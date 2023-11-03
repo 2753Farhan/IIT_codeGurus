3d43224feb3eec9beeb3cdc13fe7caeb18ce1a95
Abir-Ashab <bsse1315@iit.du.ac.bd>
2023-11-03 16:21:46 +0600

    Updated

--- apiproject/myapp/urls.py
+++ apiproject/myapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from . import views
+from . import views, views2
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index, name='index'),
+    path('city/<str:state>/<str:country>/<str:city>/', views2.city_detail, name='city_detail'),
 ]
--- apiproject/myapp/views.py
+++ apiproject/myapp/views.py
@@ -1,13 +1,27 @@
 import requests
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def index(request):
+    city_data = None
+    city_list = None
+
     if request.method == 'POST':
-        state = request.POST.get('state')  # Get user input for state
-        country = request.POST.get('country')  # Get user input for country
+        state = request.POST.get('state')
+        country = request.POST.get('country')
+        city = request.POST.get('city')
+
         YOUR_API_KEY = '31b55c52-7915-491e-b692-7df26bd172ba'
-        URL = f'http://api.airvisual.com/v2/cities?state={state}&country={country}&key={YOUR_API_KEY}'
-        r = requests.get(url=URL)
-        res = r.json()
-        return render(request, 'airapp/index.html', {'response': res, 'state': state, 'country': country})
-    return render(request, 'airapp/index.html', {'response': None})
+
+        # if not city:
+            # Step 1: Retrieve the list of cities based on state and country
+        city_list_url = f'http://api.airvisual.com/v2/cities?state={state}&country={country}&key={YOUR_API_KEY}'
+        r = requests.get(url=city_list_url)
+        city_list = r.json()
+    # else:
+        # Step 2: Retrieve city-specific air quality data
+        city_data_url = f'http://api.airvisual.com/v2/city?city={city}&state={state}&country={country}&key={YOUR_API_KEY}'
+        r = requests.get(url=city_data_url)
+        city_data = r.json()
+
+    return render(request, 'airapp/index.html', {'city_list': city_list, 'city_data': city_data})
