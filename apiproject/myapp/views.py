116b2be8bc39d3be84ccd80e9c7718558df8a041
Abir-Ashab <bsse1315@iit.du.ac.bd>
2023-11-03 15:09:10 +0600

    updated

--- apiproject/myapp/views.py
+++ apiproject/myapp/views.py
@@ -2,9 +2,12 @@ import requests
 from django.shortcuts import render
 
 def index(request):
-    YOUR_API_KEY = '31b55c52-7915-491e-b692-7df26bd172ba'
-    URL = f'http://api.airvisual.com/v2/city?city=Los%20Angeles&state=California&country=USA&key={YOUR_API_KEY}'
-    
-    r = requests.get(url=URL)
-    res = r.json()
-    return render(request, 'airapp/index.html', {'response': res})
+    if request.method == 'POST':
+        state = request.POST.get('state')  # Get user input for state
+        country = request.POST.get('country')  # Get user input for country
+        YOUR_API_KEY = '31b55c52-7915-491e-b692-7df26bd172ba'
+        URL = f'http://api.airvisual.com/v2/cities?state={state}&country={country}&key={YOUR_API_KEY}'
+        r = requests.get(url=URL)
+        res = r.json()
+        return render(request, 'airapp/index.html', {'response': res, 'state': state, 'country': country})
+    return render(request, 'airapp/index.html', {'response': None})
